6ede427b909be7fbdd5e65e2b7cc2bea629e9d67
Restyled.io <commits@restyled.io>
2023-02-04 01:36:34 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,40 +11,41 @@ delete colors.coolGray;
 delete colors.blueGray;
 
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/layouts/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      screens: {
-        sm: '500px',
-        md: '800px',
-        lg: '1200px',
-        xl: '1536px',
+  content :
+          [
+            './src/app/**/*.{js,ts,jsx,tsx}',
+            './src/pages/**/*.{js,ts,jsx,tsx}',
+            './src/components/**/*.{js,ts,jsx,tsx}',
+            './src/layouts/**/*.{js,ts,jsx,tsx}',
+          ],
+  theme : {
+    extend : {
+      screens : {
+        sm : '500px',
+        md : '800px',
+        lg : '1200px',
+        xl : '1536px',
       },
-      colors: {
+      colors : {
         ...colors,
-        brand: {
-          accent: '#5AD769',
-          main: '#324BC3',
+        brand : {
+          accent : '#5AD769',
+          main : '#324BC3',
         },
-        text: {
-          primary: '#FFFFFF',
-          secondary: '#0C0F19',
+        text : {
+          primary : '#FFFFFF',
+          secondary : '#0C0F19',
         },
-        background: {
-          primary: '#0C0F19',
+        background : {
+          primary : '#0C0F19',
         },
       },
-      fontFamily: {
-        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
-        mono: [...defaultTheme.fontFamily.mono],
-        poppins: ['var(--font-poppins)'],
+      fontFamily : {
+        sans : [ 'var(--font-inter)', ...defaultTheme.fontFamily.sans ],
+        mono : [...defaultTheme.fontFamily.mono ],
+        poppins : [ 'var(--font-poppins)' ],
       },
     },
   },
-  plugins: [],
+  plugins : [],
 };
