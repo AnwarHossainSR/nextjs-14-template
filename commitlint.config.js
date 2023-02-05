55b5f7979a34129f5fff8d167b41953ab7d77ed7
AnwarHossainSR <mahedianwar@gmail.com>
2023-02-05 11:37:14 +0600

    fix: commitlint lf to crlf issue

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,35 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+
   rules: {
-    'header-max-length': [2, 'always', 50],
+    'header-max-length': [2, 'always', 70],
+
     'subject-case': [2, 'always', 'lower-case'],
+
     'type-case': [2, 'always', 'lower-case'],
+
     'type-enum': [
       2,
+
       'always',
+
       [
         'feat',
+
         'fix',
+
         'docs',
+
         'style',
+
         'refactor',
+
         'perf',
+
         'test',
+
         'chore',
+
         'revert',
       ],
     ],
