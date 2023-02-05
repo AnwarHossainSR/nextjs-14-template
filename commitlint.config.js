0e920d913ca4e6de4ecd94ae5738aa796f5bb57d
AnwarHossainSR <mahedianwar@gmail.com>
2023-02-05 11:34:36 +0600

    chore: changes commitlint rules for commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 50],
+    'subject-case': [2, 'always', 'lower-case'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+};
