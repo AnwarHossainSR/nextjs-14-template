9bcba1ae39636b1fc82c46937b08e828e7c864bb
AnwarHossainSR <mahedianwar@gmail.com>
2023-02-05 10:49:03 +0600

    FIxed fome issue

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import type { ReactElement, ReactNode } from 'react';
+import type { ReactNode } from 'react';
 
 export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout?: () => ReactNode;
 };
 
 export type AppPropsWithLayout = AppProps & {
