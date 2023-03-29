b9bde487f2f423024b5ff47672fd5eccab1c7d09
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-03-29 12:12:04 +0600

    feat: added models

---
+++ src/models/user.model.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+export const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
