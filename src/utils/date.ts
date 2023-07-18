e848be031a048c2696d8c4b3815298200878d6f1
Anwar Hosain <anwar.sarker@bjitgroup.com>
2023-07-18 12:03:13 +0600

    chore: update dependencies

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,29 +1,63 @@
-import {
-  differenceInDays,
-  differenceInHours,
-  differenceInMinutes,
-  differenceInMonths,
-  differenceInSeconds,
-  differenceInYears,
-} from 'date-fns';
+import ms from 'ms';
 
-export const getDateCompare = (after: Date) => {
-  const currDate = new Date();
-  const compareInMinutes = differenceInMinutes(currDate, after);
-  if (compareInMinutes < 1) {
-    return `${differenceInSeconds(currDate, after)} seconds ago`;
+export const timeAgo = (timestamp: Date, timeOnly?: boolean): string => {
+  if (!timestamp) return 'never';
+  return `${ms(Date.now() - new Date(timestamp).getTime())}${
+    timeOnly ? '' : ' ago'
+  }`;
+};
+
+export async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init);
+
+  if (!res.ok) {
+    const json = await res.json();
+    if (json.error) {
+      const error = new Error(json.error) as Error & {
+        status: number;
+      };
+      error.status = res.status;
+      throw error;
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
   }
-  if (compareInMinutes < 60) {
-    return `${compareInMinutes} minutes ago`;
-  }
-  if (compareInMinutes < 60 * 24) {
-    return `${differenceInHours(currDate, after)} hour(s) ago`;
-  }
-  if (compareInMinutes < 60 * 30 * 24) {
-    return `${differenceInDays(currDate, after)} day(s) ago`;
-  }
-  if (compareInMinutes < 60 * 30 * 24 * 12) {
-    return `${differenceInMonths(currDate, after)} month(s) ago`;
-  }
-  return `${differenceInYears(currDate, after)} year(s) ago`;
+
+  return res.json();
+}
+
+export function nFormatter(num: number, digits?: number) {
+  if (!num) return '0';
+  const lookup = [
+    { value: 1, symbol: '' },
+    { value: 1e3, symbol: 'K' },
+    { value: 1e6, symbol: 'M' },
+    { value: 1e9, symbol: 'G' },
+    { value: 1e12, symbol: 'T' },
+    { value: 1e15, symbol: 'P' },
+    { value: 1e18, symbol: 'E' },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const res = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return res
+    ? (num / res.value).toFixed(digits || 1).replace(rx, '$1') + res.symbol
+    : '0';
+}
+
+export function capitalize(str: string) {
+  if (!str || typeof str !== 'string') return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export const truncate = (str: string, length: number) => {
+  if (!str || str.length <= length) return str;
+  return `${str.slice(0, length)}...`;
 };
